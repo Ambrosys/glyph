5584042761cda98be8e03a2d3f725772ce834363
mq <mquade@uni-potsdam.de>
2017-01-19 17:26:03 -0800

    fix test to account fo unused constants

--- tests/unittest/test_assessment.py
+++ tests/unittest/test_assessment.py
@@ -1,6 +1,7 @@
 """Tests for module control.gp.algorithms."""
 
 import re
+import operator
 
 import pytest
 import numpy
@@ -8,6 +9,7 @@ import dill
 
 from glyph import gp
 from glyph import assessment
+from glyph.utils.numeric import rms
 
 
 class SingleConstIndividual(gp.AExpressionTree):
@@ -27,7 +29,31 @@ class UnlimitedConstants(gp.AExpressionTree):
     marker = "symc"
 
 
-# TODO(jg): Add more test expressions.
+class Any(int):
+    def __init__(self):
+        self = 1.0
+
+    def __eq__(self, other):
+        """Equality is always true."""
+        return True
+
+    def __add__(self, other):
+        return 0
+
+
+def assert_all_close(a, b, r_tol=1e-6):
+    """Custom all_close to account for comparisons to Any()"""
+    def tupleize(x):
+        try:
+            iter(x)
+        except:
+            x = x,
+        return x
+    a = tupleize(a)
+    b = tupleize(b)
+    assert all([abs(p+operator.neg(q)) <= r_tol for p, q in zip(a, b)])
+
+
 const_opt_agreement_cases = [
     (SingleConstIndividual, 'x_0', lambda x: x, numpy.linspace(0, 100, 100), 1.0, 1),
     (SingleConstIndividual, 'Mul(c, x_0)', lambda x: x, numpy.linspace(0, 100, 100), 1.0, 1),
@@ -35,10 +61,8 @@ const_opt_agreement_cases = [
     (SingleConstIndividual, 'Add(c, Mul(c, x_0))', lambda x: 8.0 + 8.0 * x, numpy.linspace(0, 100, 100), 8.0, 1),
     (SingleConstIndividual, 'Add(c, x_0)', lambda x: 5.5 + 5.5 * x, numpy.linspace(-100, 100, 200), 5.5, 1),
     (TwoConstIndividual, 'x_0', lambda x: x, numpy.linspace(0, 100, 100), 1.0, 2),
-    (TwoConstIndividual, 'Mul(c_1, x_0)', lambda x: 2.0 * x, numpy.linspace(0, 100, 100), (1.0, 2.0), 2),
+    (TwoConstIndividual, 'Mul(c_1, x_0)', lambda x: 2.0 * x, numpy.linspace(0, 100, 100), (Any(), 2.0), 2),
     (TwoConstIndividual, 'Add(Mul(c_0, x_0), c_1)', lambda x: 4.1 * x + 2.3, numpy.linspace(0, 100, 100), (4.1, 2.3), 2),
-    # (TwoConstIndividual, 'Mul(c_0, exp(c_1, Neg(x_0))', lambda x: 3.4 * numpy.exp(-1.5 * x), numpy.linspace(0, 100, 100), (3.4, 1.5)),
-    # (TwoConstIndividual, 'Add(c_0, exp(c_1, x_0))', lambda x: 8.0 + numpy.exp(1.4 * x), numpy.linspace(0, 100, 100), (8.0, 1.4)),
     (UnlimitedConstants, 'Mul(Symc, x_0)', lambda x: 1.5 * x, numpy.linspace(0, 100, 100), 1.5, 1),
     (UnlimitedConstants, 'Mul(Symc, Add(x_0, Symc)', lambda x: x + 2.0, numpy.linspace(0, 100, 100), (1.0, 2.0), 2),
     (UnlimitedConstants, 'x_0', lambda x: x, numpy.linspace(0, 100, 100), (), 0),
@@ -55,15 +79,27 @@ class Measure:
         func = phenotype(individual)
         return func(self.x, *fargs) - self.target(self.x)
 
-
 @pytest.mark.parametrize('case', const_opt_agreement_cases)
-def test_const_opt_scalar_agreement(case):
+def test_const_opt_leastsq(case):
     individual_class, expr, target, x, desired, _ = case
     ind = individual_class.from_string(expr)
     m = Measure(target, x)
     popt, _ = assessment.const_opt_leastsq(m, ind)
-    numpy.testing.assert_allclose(actual=popt, desired=desired, rtol=1e-6)
+    assert_all_close(desired, popt)
 
+
+@pytest.mark.parametrize('case', const_opt_agreement_cases)
+def test_const_opt_scalar(case):
+    individual_class, expr, target, x, desired, _ = case
+    ind = individual_class.from_string(expr)
+    m = Measure(target, x)
+
+    def error(individual, *fargs):
+        residuals = m(individual, *fargs)
+        return rms(residuals)
+    r_tol = 1e-6
+    popt, _ = assessment.const_opt_scalar(error, ind)
+    assert_all_close(desired, popt)
 
 @pytest.mark.parametrize('case', const_opt_agreement_cases)
 def test_default_constants(case):
