df9ed655ab3075176ca776d28b80ab455ece85d4
mq <mquade@uni-potsdam.de>
2017-05-29 12:50:17 -0700

    fix kwargs

--- tests/integration_test/test_examples.py
+++ tests/integration_test/test_examples.py
@@ -46,7 +46,7 @@ def test_glyph_remote():
     with tempdir() as dirpath:
         example = os.path.abspath(os.path.join(THIS_FILES_DIR, "../../examples/remote/experiment.py"))
         exp = subprocess.Popen("python {}".format(example), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        gp = subprocess.Popen("glyph-remote --remote --ndim 2 -n 2 -p 4 --max-iter-total 1", shell=True)
+        gp = subprocess.Popen("glyph-remote --remote --ndim 2 -n 2 -p 4 --max_iter_total 1", shell=True)
 
         gp.wait()  # gp sends shutdown to exp process
         exp.wait()
